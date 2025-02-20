fc599df546ed1b6698f4ce432ab61181b52217c8
mjkhader <majd.k.khader@gmail.com>
2025-02-20 11:42:24 +0300

    adding a public place and edit the file

--- src/components/public/NavBar.vue
+++ src/components/public/NavBar.vue
@@ -2,22 +2,23 @@
   <nav class="bg-sky-blue shadow-md px-4 py-3">
     <div class="flex justify-between items-center mx-auto container">
       <!-- Logo -->
+      <router-link to="/">
       <div class="font-bold text-navy text-2xl">
         <span class="text-teal">Si</span>trip
       </div>
-      
+    </router-link>
       <!-- Navigation Links -->
       <ul class="flex space-x-6 text-navy">
-        <li><a href="#" class="hover:text-teal">Home</a></li>
-        <li><a href="#destinations" class="hover:text-teal">Destinations</a></li>
-        <li><a href="#categories" class="hover:text-teal">Categories</a></li>
+        <li><router-link to="/" class="hover:text-teal">Home</router-link></li>
+        <li><router-link to="/places" class="hover:text-teal">Places</router-link></li>
+        <li><router-link to="/hotels" class="hover:text-teal">Hotels</router-link></li>
         <li><a href="#about" class="hover:text-teal">About</a></li>
       </ul>
       
       <!-- Sign Up / Log In Buttons -->
       <div class="flex space-x-4">
-        <button class="px-4 py-2 rounded font-semibold text-navy hover:text-teal transition">Log In</button>
-        <button class="bg-teal hover:bg-sky-blue px-4 py-2 rounded-full font-semibold text-white hover:text-navy transition">Sign Up</button>
+        <button @click="$router.push('/login')" class="px-4 py-2 rounded font-semibold text-navy hover:text-teal transition">Log In</button>
+        <button @click="$router.push('/register')" class="bg-teal hover:bg-sky-blue px-4 py-2 rounded-full font-semibold text-white hover:text-navy transition">Sign Up</button>
       </div>
     </div>
   </nav>
